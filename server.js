bc1ee8cbf34c6f035c7ee353f2f231de491db806
oiacow <oiacow@gmail.com>
2019-02-08 03:37:53 -0200

    add a simple login page

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 80
+app.get('/', (req, res) => res.send('Server Online'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
