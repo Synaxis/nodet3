4e3270a2c74e3a3289884b382b4d1dd7f175b414
oiacow <oiacow@gmail.com>
2019-02-08 03:53:26 -0200

    add docker with mysql-pma

--- login.js
+++ login.js
@@ -5,6 +5,16 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = 80
+
+//set sql parameters
+var dbConn = mysql.createConnection({
+    host:   '172.18.0.1', //TODO get docker ip automatically
+    user:   'root',
+    password: '1234',
+    database: 'testLogin'
+});
+
+
 app.get('/', (req, res) => res.send('Server Online'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
